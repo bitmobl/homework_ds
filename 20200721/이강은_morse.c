4452399af3dac7bfc9b950aeeb54eb67e5f460bf
leeka <kul0328@naver.com>
2020-07-21 16:44:12 +0900

    morsecode

---
+++ 20200721/이강은_morse.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+
+void main() {
+	char a;
+	printf("\nÀÔ·Â : \n");
+	scanf_s("%c", &a);
+
+		if (a == 'a' || a == 'A') {
+			printf(".-");
+		}
+		if (a == 'b' || a == 'B') {
+			printf("-...");
+		}
+		if (a == 'c' || a == 'C') {
+			printf("-.-.");
+		}
+		if (a == 'd' || a == 'D') {
+			printf("-..");
+		}
+		if (a == 'e' || a == 'E') {
+			printf(".");
+		}
+		if (a == 'f' || a == 'F') {
+			printf("..-.");
+		}
+		if (a == 'g' || a == 'G') {
+			printf("--.");
+		}
+		if (a == 'h' || a == 'H') {
+			printf("....");
+		}
+		if (a == 'i' || a == 'I') {
+			printf("..");
+		}
+		if (a == 'j' || a == 'J') {
+			printf(".---");
+		}
+		if (a == 'k' || a == 'K') {
+			printf("-.-");
+		}
+		if (a == 'l' || a == 'L') {
+			printf(".-..");
+		}
+		if (a == 'm' || a == 'M') {
+			printf("--");
+		}
+		if (a == 'n' || a == 'N') {
+			printf("-.");
+		}
+		if (a == 'o' || a == 'O') {
+			printf("---");
+		}
+		if (a == 'p' || a == 'P') {
+			printf(".--.");
+		}
+		if (a == 'q' || a == 'Q') {
+			printf("--.-");
+		}
+		if (a == 'r' || a == 'R') {
+			printf(".-.");
+		}
+		if (a == 's' || a == 'S') {
+			printf("...");
+		}
+		if (a == 't' || a == 'T') {
+			printf("-");
+		}
+		if (a == 'u' || a == 'U') {
+			printf("..-");
+		}
+		if (a == 'v' || a == 'V') {
+			printf("...-");
+		}
+		if (a == 'w' || a == 'W') {
+			printf(".--");
+		}
+		if (a == 'x' || a == 'X') {
+			printf("-..-");
+		}
+		if (a == 'y' || a == 'Y') {
+			printf("-.--");
+		}
+		if (a == 'z' || a == 'Z') {
+			printf("--..");
+		}
+		if (a == '¤¡') {
+			printf("--..");
+		}
+	}
